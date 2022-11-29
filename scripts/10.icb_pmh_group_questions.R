f354a833229cf1e4ff9a2a4167b2bd4c627723fa
Sunderland Nicholas (BNSSG) <Nicholas.Sunderland@BNSSGCCG.NHS.UK>
2022-11-29 14:36:41 +0000

    looking at PCN rates of lipid prescribing

--- scripts/10.icb_pmh_group_questions.R
+++ scripts/10.icb_pmh_group_questions.R
@@ -9,53 +9,168 @@ load_all()
 # Connect to the server
 svr <- icdb::server("XSW")
 
-# Set some parameters for the cohort
-cohort_start     <- as.POSIXct("2018-07-01", tz = "GMT")
-cohort_end       <- as.POSIXct("2021-12-31", tz = "GMT")
-acs_codes        <- acsprojectns::get_codes(file_path = system.file("icd10_codes/acs_codes.csv", package = "acsprojectns"), append_character = "-")
-bnssg_ccg_codes  <- acsprojectns::get_codes(file_path = system.file("ccg_provider_codes/bnssg_ccg_provider_codes.csv", package = "acsprojectns"))
-adm_method_codes <- acsprojectns::get_codes(file_path = system.file("admission_method_codes/admission_method_codes.csv", package = "acsprojectns"))
-emergency_codes  <- adm_method_codes$admission_method_code[grep("emergency", adm_method_codes$admission_method_description)]
+# Some cohort parameters
+start <- "2018-07-01"
+end   <- "2021-12-31"
+adm   <- acsprojectns::get_codes(file_path = system.file("admission_method_codes/admission_method_codes.csv", package = "acsprojectns"))
+ccg   <- acsprojectns::get_codes(file_path = system.file("ccg_provider_codes/bnssg_ccg_provider_codes.csv", package = "acsprojectns"))
 
-# Get the cohort of patients
+# Get the first ACS episode within the time window
 sus <- svr$ABI$vw_APC_SEM_001 %>%
   select(AIMTC_Pseudo_NHS,
+         AIMTC_PCTRegGP,
+         AIMTC_PracticeCodeOfRegisteredGP,
+         AdmissionMethod_HospitalProviderSpell,
          AIMTC_ProviderSpell_Start_Date,
          AIMTC_ProviderSpell_End_Date,
-         AdmissionMethod_HospitalProviderSpell,
-         AIMTC_PCTRegGP,
-         matches("Diagnosis[0-9]*[rdths]*(Primary|Secondary)_ICD")) %>%
-  filter(AIMTC_ProviderSpell_Start_Date >= cohort_start,
-         AIMTC_ProviderSpell_Start_Date <= cohort_end,
-         AdmissionMethod_HospitalProviderSpell %in% !!emergency_codes,
-         AIMTC_PCTRegGP %in% !!bnssg_ccg_codes$ccg_provider_code,
-         if_any(matches("Diagnosis[0-9]*[rdths]*(Primary)_ICD"), ~ .x %in% !!acs_codes$icd_codes)) %>%
+         DiagnosisPrimary_ICD) %>%
+  filter(AIMTC_ProviderSpell_Start_Date > start,
+         AIMTC_ProviderSpell_End_Date   < end,
+         AdmissionMethod_HospitalProviderSpell %in% !!adm$admission_method_code[grep("emergency", adm$admission_method_description)],
+         AIMTC_PCTRegGP %in% !!ccg$ccg_provider_code) %>%
+  codes_from("icd10/acs.yaml", DiagnosisPrimary_ICD) %>%
   group_by(AIMTC_Pseudo_NHS) %>%
-  slice_min(AIMTC_ProviderSpell_Start_Date, with_ties=F) %>%
-  show_query() %>%
-  run()
-
-
-# The PCN for each patient
-swd_att <- svr$MODELLING_ANALYTICS$swd_attributes_history %>%
-  select(nhs_number, attribute_period, primary_care_network, locality_name, lsoa) %>%
-  filter(nhs_number %in% !!cohort$nhs_number) %>%
-  group_by(nhs_number) %>%
-  mutate(swd_min = min(.data$attribute_period, na.rm=T),
-         swd_max = max(.data$attribute_period, na.rm=T)) %>%
+  slice_min(AIMTC_ProviderSpell_Start_Date, with_ties = FALSE) %>%
   ungroup() %>%
+  show_query() %>%
+  run() %>%
+  select(nhs_number = AIMTC_Pseudo_NHS,
+         prac_code  = AIMTC_PracticeCodeOfRegisteredGP,
+         epi_start  = AIMTC_ProviderSpell_Start_Date,
+         epi_end    = AIMTC_ProviderSpell_End_Date,
+         primary_diagnosis = DiagnosisPrimary_ICD) %>%
+  mutate(primary_diagnosis = drop_detail(.data$primary_diagnosis, level=2))
+
+# The SWD date range
+in_swd <- svr$MODELLING_SQL_AREA$primary_care_attributes %>%
+  select(nhs_number, attribute_period) %>%
+  filter(nhs_number %in% !!sus$nhs_number) %>%
+  run() %>%
+  group_by(nhs_number) %>%
+  mutate(swd_min = min(attribute_period, na.rm=T),
+         swd_max = max(attribute_period, na.rm=T)) %>%
+  select(nhs_number, swd_min, swd_max) %>%
+  slice(n=1) %>%
+  ungroup()
+
+# Flag people who don't have SWD data
+cohort <- sus %>%
+  left_join(in_swd, by="nhs_number") %>%
+  mutate(in_swd = if_else(!is.na(swd_min),
+                          lubridate::int_overlaps(lubridate::interval(epi_start, epi_end+lubridate::duration(1,"years")),
+                                                  lubridate::interval(swd_min, swd_max)),
+                          FALSE))
+cat("Number ACS patients: ", as.character(nrow(cohort)))
+cat("Number with SWD data:", as.character(sum(cohort$in_swd)), ",", as.character(round(sum(cohort$in_swd)*100/nrow(cohort)),digits=0), "%")
+
+# The PCN for each practice
+prac_pcn <- svr$MODELLING_SQL_AREA$swd_attribute %>%
+  distinct(practice_code, primary_care_network) %>%
+  mutate(practice_code = toupper(practice_code)) %>%
+  select(pcn=primary_care_network, prac_code=practice_code) %>%
   run()
 
+# Add PCN to cohort
+cohort <- cohort %>%
+  left_join(prac_pcn, by="prac_code") %>%
+  relocate(pcn, .after=prac_code) %>%
+  mutate(across(c(prac_code, pcn, primary_diagnosis), ~as.factor(.x)))
 
-# Statin prescriptions
-swd_drugs <- svr$MODELLING_ANALYTICS$swd_activity %>%
-  select(nhs_number, pod_l1) %>%
-  filter(nhs_number %in% !!cohort$nhs_number,
+# Look at who has SWD data by practice
+ggplot(data    = cohort %>% group_by(pcn) %>% summarise(n=n(), pct_swd=sum(in_swd)/n()) %>% ungroup(),
+       mapping = aes(x=forcats::fct_reorder(pcn, pct_swd, .desc=T), y=pct_swd)) +
+  geom_bar(stat="identity", fill="lightblue") +
+  geom_text(aes(label=n), vjust=0.4, hjust=2) +
+  coord_flip() +
+  ylab('Percent of ACS patients with follow up SWD data') +
+  scale_y_continuous(labels = scales::percent)
+
+# Remove those without SWD data
+cohort <- cohort %>% filter(in_swd)
+
+# The prescriptions
+med_filter_string <- "(?i)([A-z ]+)(\\d+\\.?\\d*)?([A-z]+)?[ ]*(?:[A-z]*)"
+search_meds = c("inclisiran", "ezetimibe", "atorvastatin", "rosuvastatin", "simvastatin", "fluvastatin", "pravastatin")
+med_end = as.Date(end) + lubridate::duration(1, units="years")
+meds <- svr$MODELLING_SQL_AREA$swd_activity %>%
+  select(nhs_number, arr_date, dep_date, pod_l1, spec_l1b) %>%
+  filter(nhs_number %in% !!sus$nhs_number,
+         arr_date > start,
+         dep_date < med_end,
          pod_l1 == "primary_care_prescription") %>%
+  show_query() %>%
   run() %>%
+  filter(grepl(paste0(search_meds, collapse = "|"), spec_l1b, ignore.case=T)) %>%
+  dplyr::mutate(med_name  = trimws(stringr::str_match(spec_l1b, pattern = med_filter_string)[,2]),
+                med_dose  = stringr::str_match(spec_l1b, pattern = med_filter_string)[,3],
+                med_units = stringr::str_match(spec_l1b, pattern = med_filter_string)[,4]) %>%
+  select(nhs_number,
+         prescription_date = arr_date,
+         med_name,
+         med_dose,
+         med_units)
+
+# Filter the meds to within 1 year of ACS
+meds <- sus %>%
+  left_join(meds, by="nhs_number") %>%
+  filter(prescription_date > epi_start,
+         prescription_date < epi_end + lubridate::duration(1, "years")) %>%
+  select(nhs_number, prescription_date, med_name, med_dose, med_units)
+
+# Percentage of people with lipid therapy after ACS
+cohort_meds <- cohort %>%
+  left_join(meds, by="nhs_number") %>%
+  dplyr::mutate(strategy = dplyr::case_when(
+    is.na(med_name)                               ~ "None",
+    med_name=="inclisiran"                        ~ "Inclisiran",
+    (med_name=="atorvastatin" & med_dose >=40) |
+    (med_name=="rosuvastatin" & med_dose >=20) |
+    (med_name=="simvastatin"  & med_dose >=80)    ~ "High intensity",
+    TRUE                                          ~ "Low or Medium Intensity")) %>%
+  group_by(nhs_number) %>%
+  mutate(strategy_mode  = tail(names(sort(table(strategy))), 1),
+         strategy_mode = factor(strategy_mode, levels=c("None","Low or Medium Intensity","High intensity","Inclisiran")),
+         num_s = n(),
+         num_scripts = case_when(all(is.na(med_name))~"0",
+                                 num_s<3  ~ "1-2",
+                                 num_s<7  ~ "3-6",
+                                 num_s<13 ~ "7-12",
+                                 num_s<25 ~ "13-24",
+                                 num_s>=25~ "25+"),
+         num_scripts = factor(num_scripts, levels=c("0","1-2","3-6","7-12","13-24","25+"))) %>%
+  slice(n=1) %>%
+  select(-strategy) %>%
+  group_by(pcn) %>%
+  mutate(num_none = sum(strategy_mode=="None")) %>%
+  ungroup()
+
+# Pie chart of strategies
+ggplot(cohort_meds, aes(x=factor(1), fill=strategy_mode))+
+  geom_bar(width = 1) +
+  coord_polar("y")
+
+# Plot the % people with at least one prescription over the year
+ggplot(data    = cohort_meds,
+       mapping = aes(x=pcn, fill=forcats::fct_rev(strategy_mode))) +
+  geom_bar(position="fill") +
+  coord_flip() +
+  ylab('Percent of ACS patients with at least one prescription')
+
+# Plot the % people with at least 6 prescriptions over the year
+ggplot(data    = cohort_meds,
+       mapping = aes(x=pcn, fill=forcats::fct_rev(num_scripts))) +
+  geom_bar(position="fill") +
+  coord_flip() +
+  ylab('Percent of ACS patients with different number of prescriptions')
 
 
-  search_meds = c("inclisiran", "ezetimibe", "atorvastatin", "rosuvastatin", "simvastatin", "fluvastatin", "pravastatin")
+
+
+
+
+
+
+
 
 # Create the timeseries to plot
 lipid_medication_timeseries <- create_medication_timeseries(cohort_df=cohort, meds_df=meds, t_start, t_end, step, window_span, t_units, search_meds)
