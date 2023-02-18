e9752d354d476f4c648c8b3a60b999b143975adc
nicksunderland <nicholas.p.sunderland@gmail.com>
2023-02-18 23:32:15 +0000

    set up home mysql database and created synthetic data

--- scripts/create_dummy_database_scripts/write_testing_sqlite_database.R
+++ scripts/create_dummy_database_scripts/write_testing_sqlite_database.R
@@ -1,8 +1,39 @@
-library(RSQLite)
-data("mtcars")
-mtcars$car_names <- rownames(mtcars)
-rownames(mtcars) <- c()
-head(mtcars)
-conn <- dbConnect(RSQLite::SQLite(), file.path(system.file("extdata", package="acsprojectns"), "testing_db.db"))
-dbWriteTable(conn, "test1", mtcars, overwrite=T)
-dbListTables(conn)
+# Prerequisites
+# download and set up MySql for mac - 'brew install mysql'
+# download odbc manager for mac - 'brew install --cask odbc-manager'
+# download the MySql driver / Connector/ODBC 8.0.32 - https://dev.mysql.com/downloads/connector/odbc/
+# download mysqlworkbench - brew install --cask mysqlworkbench
+# create the schema to match the ICD database e.g. "ABI" etc.
+# open ODBC manager and create a DSN named "XSW" to match the ICB; add your MySql 'user' and 'password'
+# also create and set a variable called 'NO_SCHEMA' to 'false'
+# https://dev.mysql.com/doc/connector-odbc/en/connector-odbc-usagenotes-functionality-catalog-schema.html
+# https://solutions.posit.co/connections/db/best-practices/schema/
+
+# imports
+library(DBI)
+devtools::load_all()
+
+# connect to mysql server
+conn <- DBI::dbConnect(odbc::odbc(), "XSW")
+
+# data tables; table_name: file_name
+data_tables = list(
+  list("catalog"="ABI",                "schema"="dbo", "name"="vw_APC_SEM_001",          "data"="vw_APC_SEM_001_dummy_database.csv"),
+  list("catalog"="ABI",                "schema"="dbo", "name"="vw_AE_SEM_001",           "data"="vw_AE_SEM_001_dummy_database.csv"),
+  list("catalog"="ABI",                "schema"="dbo", "name"="vw_NHAIS_Deaths_All",     "data"="vw_NHAIS_Deaths_All_dummy_database.csv"),
+  list("catalog"="ABI",                "schema"="dbo", "name"="Mortality",               "data"="CivilMortality_dummy_database.csv"),
+  list("catalog"="MODELLING_SQL_AREA", "schema"="dbo", "name"="primary_care_attributes", "data"="swd_attributes_history_dummy_database.csv"),
+  list("catalog"="MODELLING_SQL_AREA", "schema"="dbo", "name"="swd_attribute",           "data"="swd_attributes_dummy_database.csv"),
+  list("catalog"="MODELLING_SQL_AREA", "schema"="dbo", "name"="swd_activity",            "data"="swd_activity_dummy_database.csv"),
+  list("catalog"="MODELLING_SQL_AREA", "schema"="dbo", "name"="swd_LSOA_descriptions",   "data"="swd_lsoa_dummy_database.csv"),
+  list("catalog"="MODELLING_SQL_AREA", "schema"="dbo", "name"="swd_measurement",         "data"="swd_measurement_dummy_database.csv")
+)
+
+for(table in data_tables){
+  fp <- system.file("materials", "dummy_databases", table$data, package="acsprojectns")
+  d  <- readr:: read_csv(fp, show_col_types=F)
+  n  <- paste(table$catalog, table$name, sep=".")
+  DBI::dbWriteTable(conn, DBI::SQL(n), d, overwrite=T)
+}
+
+DBI::dbDisconnect(conn)
